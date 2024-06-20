e292b6110314e5262a4e8b4d7fe8147fa0d0e504
irfan <haque.irfanul.m@gmail.com>
2024-06-20 09:25:48 +0600

    Added template and basics | TODO: CV and Portfolio

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'M Irfanul Haque';
+export const SITE_DESCRIPTION = 'A personal website for a human';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
+export const FULL_NAME = "Muhammad Irfanul Haque"
+export const SITE_URL = "https://mi897.github.io"
+export const GITHUB_PROFILE = "https://github.com/mi897"
+export const LINKEDIN_PROFILE = "https://www.linkedin.com/in/irfan-haque/"
+export const EMAIL = "haque.irfanul.m@gmail.com"
