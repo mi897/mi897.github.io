0c16ba1683cb3f00fabbe2b044e73e3e432f2955
mi8 <haque.irfanul.m@gmail.com>
2026-06-25 15:09:47 +0600

    update site title and URLs to reflect new domain

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'mi8.space';
+export const SITE_TITLE = 'mi8.studio';
 export const SITE_DESCRIPTION = 'A space to be';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
 export const FULL_NAME = "Muhammad Irfanul Haque"
-export const SITE_URL = "https://mi8.space"
+export const SITE_URL = "https://mi8.studio"
 export const GITHUB_PROFILE = "https://github.com/mi897"
 export const LINKEDIN_PROFILE = "https://www.linkedin.com/in/irfan-haque/"
-export const EMAIL = "irfan@mi8.space"
+export const EMAIL = "irfan@mi8.studio"
